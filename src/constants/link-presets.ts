319db59c6a5fd0703e5f69dce9c782c6b4c8d7c2
xzxljt <1941689601@qq.com>
2026-02-12 18:22:52 +0000

    删除导航栏中的统计和状态页面链接

--- src/constants/link-presets.ts
+++ src/constants/link-presets.ts
@@ -21,16 +21,6 @@ export const LinkPresets: { [key in LinkPreset]: NavBarLink } = {
 		name: "赞助",
 		url: "/donate/",
 	},
-	[LinkPreset.Stats]: {
-		name: "统计",
-		url: "https://umami.312522.xyz/share/X9ZZZ5l2xErS44Rc",
-		external: true,
-	},
-	[LinkPreset.Status]: {
-		name: "状态",
-		url: "https://uptime.312522.xyz/status/default",
-		external: true,
-	},
 	[LinkPreset.Monitor]: {
 		name: "流量监控",
 		url: "https://tian.mt2.de5.net/",
