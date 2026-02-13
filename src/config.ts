b2c36637be49fb74000d2028abf6e94dedc8e342
xzxljt <1941689601@qq.com>
2026-02-13 17:25:12 +0800

    删除无用的 banner credit URL

--- src/config.ts
+++ src/config.ts
@@ -29,10 +29,10 @@ export const siteConfig: SiteConfig = {
 
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
-			enable: true, // Display the credit text of the banner image
-			text: "Pixiv @chokei", // Credit text to be displayed
+			enable: false, // Display the credit text of the banner image
+			text: "", // Credit text to be displayed
 
-			url: "https://image.cloudrunmax.top/random", // (Optional) URL link to the original artwork or artist's page
+			url: "", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
 	background: {
